c62e0b225d922b4af9cca80736d8851aa72b9424
Ernesto Alfonso <ernesto.alfonsogonzalez@ge.com>
2016-07-03 20:30:45 -0700

    proxy-to-map: leave out protocol, parse port str

--- src/cf_lib/util.clj
+++ src/cf_lib/util.clj
@@ -7,9 +7,11 @@ and not a string like http://my-proxy:8080"
   (when proxy
     (condp instance? proxy
       java.util.Map proxy
-      String (let [[_ host port] (re-matches #"^(.*):([0-9]+)$" proxy)]
+      String (let [[all protocol host port]
+                   (re-matches #"^(https?://)?(.*):([0-9]+)$"
+                               proxy)]
                {:host host
-                :port port}))))
+                :port (Integer/parseInt port)}))))
 
 (defmacro get-chain [obj & accessors]
   "(get-chain {1 {2 {3 {4 5}}}} 1 2 3) => {4 5}"
