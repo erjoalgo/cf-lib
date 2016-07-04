bb4e723533c67a72ff5c847ce9a4d8a9c2b34cbe
Ernesto Alfonso <ernesto.alfonsogonzalez@ge.com>
2016-07-04 14:47:24 -0700

    add basic project description and apache license

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
 (defproject cf-lib
-  ;"0.1.0-SNAPSHOT"
-  "1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  "0.1.0-SNAPSHOT"
+  :description "Clojure bindings for the cloud foundry api"
+  :url ""
+  :license {:name "Apache License Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "3.0.1"]
                  [org.clojure/data.json "0.2.6"]
