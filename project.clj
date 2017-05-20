feda9479e220c5782d5ddcc5bdd5d644a4f8af17
vagrant <ernesto.alfonsogonzalez@ge.com>
2017-05-19 19:36:28 -0700

    project.clj: update license and url

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject cf-lib
   "0.1.0-SNAPSHOT"
   :description "Clojure bindings for the cloud foundry api"
-  :url ""
-  :license {:name "Apache License Version 2.0"
-            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :url "https://github.build.ge.com/hubs/cf-lib"
+  :license {:name "BSD"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "3.0.1"]
                  [org.clojure/data.json "0.2.6"]
